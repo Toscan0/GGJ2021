e45e1bcdc0734db2e7ea15787e966c24933d4ff7
Marcelo Nunes <marcelojcnunes@hotmail.com>
2021-01-29 22:59:53 +0000

    feat(behaviour): Added EnterRoom MonoBehaviour;

---
+++ GGJ2021/Assets/EnterRoom.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+
+public class EnterRoom : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject NextRoom;
+
+    public bool ChangeRoom = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (ChangeRoom && NextRoom != null) 
+        {
+            var currentRoom = Camera.main.GetComponent<CurrentRoom>();
+
+            currentRoom.EnterRoom(NextRoom.gameObject.transform.position);
+
+            ChangeRoom = false;
+        }
+    }
+}
