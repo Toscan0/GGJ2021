031de46a80cd6e2921a766f3b4da89431906d63d
Marcelo Nunes <marcelojcnunes@hotmail.com>
2021-01-31 12:45:12 +0000

    - fairy sound

--- GGJ2021/Assets/Scripts/LightAllRoom/PressurePlate.cs
+++ GGJ2021/Assets/Scripts/LightAllRoom/PressurePlate.cs
@@ -6,6 +6,9 @@ public class PressurePlate : MonoBehaviour
     [SerializeField]
     private GameObject[] particles;
 
+    [SerializeField]
+    private AudioSource AudioSource;
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.tag == "Player")
@@ -13,6 +16,11 @@ public class PressurePlate : MonoBehaviour
             foreach(var ps in particles)
             {
                 ps.SetActive(true);
+            }
+
+            if (AudioSource != null)
+            {
+                AudioSource.Play();
             }
         }
     }
